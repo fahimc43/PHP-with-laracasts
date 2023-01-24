a2252309453358ff7b931a9976b582a829b1079a
Fahim Chowdhury <fahimc43@gmail.com>
2023-01-25 05:24:54 +0600

    step of fetch data

--- index.php
+++ index.php
@@ -4,19 +4,35 @@ require "functions.php";
 
 // require "router.php";
 
-class Person 
-{
-    public $name;
-    public $age;
+// class Person 
+// {
+//     public $name;
+//     public $age;
 
-    public function breathe() {
-        echo $this->name . " is breathing";
-    }
+//     public function breathe() {
+//         echo $this->name . " is breathing";
+//     }
+// }
+
+// $person = new Person();
+
+// $person->name = "Jhon lora";
+// $person->age = 30;
+
+// dd($person->breathe());
+
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4;";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
 }
 
-$person = new Person();
-
-$person->name = "Jhon lora";
-$person->age = 30;
-
-dd($person->breathe());
+// dd($posts);
